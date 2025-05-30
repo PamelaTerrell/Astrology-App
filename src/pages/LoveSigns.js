a58cd55e3e0114fb69f0bac8c7163f752d212e8e
PamelaTerrell <pammyhoney@yahoo.com>
2025-05-30 15:51:50 -0400

    Footer fix

--- src/pages/LoveSigns.js
+++ src/pages/LoveSigns.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './LoveSigns.css';
-//import Footer from '../components/Footer';
+import Footer from '../components/Footer';
 
 const LoveSigns = () => {
   return (
@@ -15,6 +15,9 @@ const LoveSigns = () => {
           I have requested permission to use selected excerpts and references from the book. 
           Once permission is granted, this section will be updated with curated insights aligned with the original text.
         </p>
+
+        <Footer />
+
       </div>
 
       
